8bd7c114024e279e27b7103672b448166a7be84f
Rory Jarrard <rjarrard@gmail.com>
2024-10-29 12:09:26 -0600

    display package details

--- src/pages/details/DetailsPage.tsx
+++ src/pages/details/DetailsPage.tsx
@@ -1,7 +1,27 @@
+import { useLoaderData } from 'react-router-dom';
+import type { DetailsLoaderResult } from './detailsLoader';
+
 const DetailsPage = () => {
+    const { details } = useLoaderData() as DetailsLoaderResult;
+
     return (
-        <div>
-            <h1>Details Page</h1>
+        <div className="space-y-4">
+            <h1 className="text-3xl font-bold my-4">{details.name}</h1>
+
+            <div>
+                <h3 className="text-lg font-bold">Description</h3>
+                <div className="p-3 bg-gray-200 rounded">{details.description}</div>
+            </div>
+
+            <div>
+                <h3 className="text-lg font-bold">License</h3>
+                <div className="p-3 bg-gray-200 rounded">{details.license}</div>
+            </div>
+
+            <div>
+                <h3 className="text-lg font-bold">Author</h3>
+                <div className="p-3 bg-gray-200 rounded">{details.author?.name}</div>
+            </div>
         </div>
     );
 };
