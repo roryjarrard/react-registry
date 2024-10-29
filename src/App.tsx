f6e8afe30ffa3f3080991164480ce5a2d97ee6d1
Rory Jarrard <rjarrard@gmail.com>
2024-10-29 08:31:56 -0600

    search npm registry

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,22 @@ const router = createBrowserRouter([
       {
         path: '/search',
         element: <SearchPage />,
+        loader: async ({ request }) => {
+          const {searchParams} = new URL(request.url);
+          const term = searchParams.get('term');
+          
+          if (!term) {
+            throw new Error('Search term must be provided');
+          }
+
+          const res = await fetch(
+            `https://registry.npmjs.com/-/v1/search?text=${term}&size=10`
+          );
+
+          const data = await res.json();
+
+          return data.objects;
+        },
       },
       {
         path: '/packages/:name',
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,4 +1,10 @@
+import { useLoaderData } from 'react-router-dom';
+
 const SearchPage = () => {
+    const data = useLoaderData();
+
+    console.log({data});
+    
     return (
         <div>
             <h1>Search Page</h1>
