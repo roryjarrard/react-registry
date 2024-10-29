cf0f75846c2ed9b734d3efc52678605616c2c081
Rory Jarrard <rjarrard@gmail.com>
2024-10-29 12:33:49 -0600

    display featured packages on Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Root from './pages/Root';
-import HomePage from './pages/HomePage';
+import HomePage from './pages/home/HomePage';
 import DetailsPage from './pages/details/DetailsPage';
 import SearchPage from './pages/search/SearchPage';
 import { searchLoader } from './pages/search/searchLoader';
 import { detailsLoader } from './pages/details/detailsLoader';
+import { homeLoader } from './pages/home/homeLoader';
 
 const router = createBrowserRouter([
   {
@@ -14,6 +15,7 @@ const router = createBrowserRouter([
       {
         index: true,
         element: <HomePage />,
+        loader: homeLoader,
       },
       {
         path: '/search',
